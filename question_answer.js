8d89a823ba655477a682dbf57fb7bfa2e738e566
Frederic-vW <fvw.github@gmail.com>
2021-08-25 17:12:01 +1000

    Add files via upload

---
+++ question_answer.js
@@ -0,0 +1,24 @@
+<script type="text/javascript">
+var questions = document.getElementsByTagName("button");
+for (var q_btn of questions) {
+  try {
+    var btnId = q_btn.getAttribute("id");
+    if (btnId.startsWith("q-")) {
+      q_btn.addEventListener("click", getBtnEvent(btnId), false)
+    }
+  } catch(e) {}
+}
+
+function getBtnEvent(btnId){
+  function f() {
+    var ans_id = btnId.replace("q-","a-");
+    var ans = document.getElementById(ans_id);
+    if (ans.style.display === "block") {
+      ans.style.display = "none";
+    } else {
+      ans.style.display = "block";
+    }
+  };
+  return f;
+}
+</script>
