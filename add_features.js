e89a24e53ab73f126c186a594e9b23a7c644d5c2
Frederic-vW <fvw.github@gmail.com>
2023-02-28 12:07:36 +1100

    Create add_features.js

---
+++ add_features.js
@@ -0,0 +1,5 @@
+link = document.createElement("link");
+link.type = "text/css";
+link.rel = "stylesheet";
+link.href = "https://frederic-vw.github.io/moodle-utils/features.css";
+document.head.appendChild(link);
